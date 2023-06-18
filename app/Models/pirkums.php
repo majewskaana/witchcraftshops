e0a88b0c8350e1793e36b5a335395b8c81611725
majewskaana <a.majevska@inbox.lv>
2023-06-18 13:23:30 +0300

    Added model's modification 	modified:   app/Models/kompanija.php 	modified:   app/Models/pirkums.php 	modified:   app/Models/smarza.php 	modified:   app/Models/valsts.php 	deleted:    app/Models/veikals.php 	modified:   app/Models/zodiaks.php

--- app/Models/pirkums.php
+++ app/Models/pirkums.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class pirkums extends Model
 {
     use HasFactory;
+    public function akmens(){
+        return $this->belongsTo(akmens::class);
+        }
+    public function garsviela(){
+        return $this->belongsTo(garsviela::class);
+        }
+    public function kartis(){
+        return $this->belongsTo(kartis::class);
+        }
+     public function razotajs(){
+        return $this->belongsTo(kompanija::class);
+        }
+
+
 }
--- app/Models/zodiaks.php
+++ app/Models/zodiaks.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class zodiaks extends Model
 {
     use HasFactory;
+    protected $fillable = ['Nosaukums'];
+    public function zodiaks(){
+        return $this->hasMany(akmens::class);
+        }
 }
