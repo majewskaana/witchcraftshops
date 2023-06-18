b17413875e36b58b08793d446944d2f7c2c865e9
majewskaana <a.majevska@inbox.lv>
2023-06-18 19:22:08 +0300

    new file:   app/Http/Controllers/StoneController.php 	new file:   resources/views/greetingandcategorychoosing.blade.php 	modified:   routes/web.php

---
+++ resources/views/greetingandcategorychoosing.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>Countries</title>
+</head>
+<body>
+ <h1>Welcome to our catalogue!</h1>
+ <p>Choose the category</p>
+
+ <p>Stone</p>
+ <p>Spices</p>
+ <p>Cards</p>
+
+</body>
+</html>
