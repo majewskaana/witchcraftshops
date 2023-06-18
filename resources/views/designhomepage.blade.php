c240d2d22f6cebd77bd84bfbe5cccd8a16dd7d9d
Dominika Kļopova <dk22075@edu.lu.lv>
2023-06-18 15:35:10 +0300

    new file:   public/images/card.png 	new file:   public/images/donate.png 	modified:   public/styles.css 	modified:   resources/views/designhomepage.blade.php

--- resources/views/designhomepage.blade.php
+++ resources/views/designhomepage.blade.php
@@ -87,26 +87,47 @@
     </div>
     <!-- More product items -->
   </section>
-  
   <footer>
-    <div class="shop-map">
-      <h3>OFFICE MAP</h3>
-      <img src="images/map.png" alt="Click Me" onclick="redirectToLink('https://www.google.com/maps/place/Aleksandra+Čaka+iela+100,+Latgales+priekšpilsēta,+Rīga,+LV-1011/@56.9571881,24.1400531,17z/data=!3m1!4b1!4m6!3m5!1s0x46eece343a2aef39:0x23a3f6aea0fc666a!8m2!3d56.9571881!4d24.1400531!16s%2Fg%2F11mtmlbf4c?entry=ttu')">
-  <script>
-    function redirectToLink(url) {
-      window.location.href = url;
-    }
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="section">
+          <h3>OFFICE MAP</h3>
+          <img src="images/map.png" alt="Click Me" onclick="redirectToLink('https://www.google.com/maps/place/Aleksandra+Čaka+iela+100,+Latgales+priekšpilsēta,+Rīga,+LV-1011/@56.9571881,24.1400531,17z/data=!3m1!4b1!4m6!3m5!1s0x46eece343a2aef39:0x23a3f6aea0fc666a!8m2!3d56.9571881!4d24.1400531!16s%2Fg%2F11mtmlbf4c?entry=ttu')">
+          <script>
+            function redirectToLink(url) {
+              window.location.href = url;
+            }
+          </script>
 
-  </script>
+          <h3>INFO</h3>
+          <p>Par mums:</p>
+          <p>Juridiskā adrese: Rīga, Aleksandra Čaka 100</p>
+          <p>Kontakti: +37128282828</p>
+          <p>Rezervācija: pa pastu witchcraft@inbox.lv</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="section">
+          <a href="https://www.donationalerts.com" class="donate-button">Donate</a>
+          <img src="images/donate.png" alt="Logo">
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="section">
+          <div class="card-image">
+            <p>YOUR CARD OF THE DAY!</p>
+            <img src="images/card.png" alt="Card">
+          </div>
+          
+        </div>
+      </div>
     </div>
-    <div class="basic-info">
-      <h3> INFO</h3>
-      <p>Par mums:</p>
-      <p>Juridiskā adrese: Rīga, Aleksandra Čaka 100</p>
-      <p>Kontakti: +37128282828</p>
-      <p>Rezervācija: pa pastu witchcraft@inbox.lv</p>
-      
-    </div>
-  </footer>
+  </div>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
 </body>
 </html>
