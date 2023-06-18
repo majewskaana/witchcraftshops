f1fa33cd0fbb5397f159dfcfe525a8f9e333c249
majewskaana <a.majevska@inbox.lv>
2023-06-18 12:06:53 +0300

    added to the my site folder 	new file:   resources/views/designhomepage.blade.php

---
+++ resources/views/designhomepage.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Online Shop</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <header>
+    <h1>WITCHCRAFT SUPPLEMENTS</h1>
+    <div class="language">
+      <a href="#" class="active">LV</a> | <a href="#">ENG</a>
+    </div>
+    <div class="user-buttons">
+    <div>
+      <a href="#" class="signup-button">Sign Up</a>
+    </div>
+    <div>
+      <a href="#" class="login-button">Log In</a>
+    </div>
+</div>
+  </header>
+  
+  <section class="filter">
+  <h2>Filter</h2>
+  <div class="filter-group">
+    <ul>
+      <li><button type="button" name="filter" value="stones">Stones</button></li>
+      <li><button type="button" name="filter" value="zodiac">Zodiac</button></li>
+    </ul>
+  </div>
+  <div class="filter-group">
+    <h3><button type="button" name="filter" value="spices">Spices</button></h3>
+    <ul>
+      <li><button type="button" name="filter" value="smell">Smell</button></li>
+    </ul>
+  </div>
+  <div class="filter-group">
+    <h3><button type="button" name="filter" value="cards">Cards</button></h3>
+    <ul>
+      <li><button type="button" name="filter" value="cards">Cards</button></li>
+    </ul>
+  </div>
+</section>
+
+
+  
+  <section class="product-catalogue">
+    <h2>Product Catalogue</h2>
+    <div class="product-item">
+      <img src="product1.jpg" alt="Product 1">
+      <h3>Product 1</h3>
+      <p>Description of Product 1</p>
+      <span class="price">$19.99</span>
+      <a href="#" class="buy-button">Buy Now</a>
+    </div>
+    <div class="product-item">
+      <img src="product2.jpg" alt="Product 2">
+      <h3>Product 2</h3>
+      <p>Description of Product 2</p>
+      <span class="price">$24.99</span>
+      <a href="#" class="buy-button">Buy Now</a>
+    </div>
+    <div class="product-item">
+      <img src="product2.jpg" alt="Product 2">
+      <h3>Product 2</h3>
+      <p>Description of Product 2</p>
+      <span class="price">$24.99</span>
+      <a href="#" class="buy-button">Buy Now</a>
+    </div>
+    <div class="product-item">
+      <img src="product2.jpg" alt="Product 2">
+      <h3>Product 2</h3>
+      <p>Description of Product 2</p>
+      <span class="price">$24.99</span>
+      <a href="#" class="buy-button">Buy Now</a>
+    </div>
+    <div class="product-item">
+      <img src="product2.jpg" alt="Product 2">
+      <h3>Product 2</h3>
+      <p>Description of Product 2</p>
+      <span class="price">$24.99</span>
+      <a href="#" class="buy-button">Buy Now</a>
+    </div>
+    <!-- More product items -->
+  </section>
+  
+  <footer>
+    <div class="shop-map">
+      <h3>SHOP MAP</h3>
+      <img src="map.jpg" alt="Shop Map">
+    </div>
+    <div class="basic-info">
+      <h3> INFO</h3>
+      <p>Par mums:</p>
+      <p>Juridiskā adrese: Rīga, Aleksandra Čaka 100</p>
+      <p>Kontakti: +37128282828</p>
+      <p>Rezervācija: pa pastu witchcraft@inbox.lv</p>
+      
+    </div>
+  </footer>
+</body>
+</html>
