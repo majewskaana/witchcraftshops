470c457cc19032441ee0b0be8b3203dee7d6fec3
majewskaana <a.majevska@inbox.lv>
2023-06-19 19:44:30 +0300

    modified:   resources/views/greetingandcategorychoosing.blade.php

---
+++ resources/views/greetingandcategorychoosing.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>Categories</title>
+</head>
+<body>
+ <h1>Welcome to our catalogue!</h1>
+ <p>Choose the category</p>
+
+ <p>Stone</p>
+ <p>Spices</p>
+ <p>Cards</p>
+
+</body>
+</html>
