4c8cf60be4c680187cee0b49d263afdd920672b6
majewskaana <a.majevska@inbox.lv>
2023-06-19 23:15:06 +0300

    modified:   resources/views/cardscatalogue.blade.php 	modified:   resources/views/spicescatalogue.blade.php 	modified:   resources/views/stonecatalogue.blade.php

--- resources/views/cardscatalogue.blade.php
+++ resources/views/cardscatalogue.blade.php
@@ -5,8 +5,28 @@
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
  <link rel="stylesheet" href="styles.css">
- <title>Cards</title>
+ <style>
+    /* Additional CSS */
+    footer .shop-map img {
+      width: 400px;
+      height: 200px;
+    }
+  </style>
+  <title>Online Shop</title>
 </head>
+<header>
+    <h1>WITCHCRAFT SUPPLEMENTS</h1>
+    <div class="language">
+      <a href="#" class="active">LV</a> | <a href="#">ENG</a>
+    </div>
+    <div class="user-buttons">
+    <div>
+      <button>Sign Up</button>
+      <button>Sign Up</button>
+
+    </div>
+</div>
+  </header>
 <body>
  
 <section class="filter">
@@ -32,5 +52,47 @@
             @endforeach
         </ul>
     @endif
+
+    <footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div id="office-map-section">
+          <h3>OFFICE MAP</h3>
+          <img src="images/map.png" alt="Click Me" onclick="redirectToLink('https://www.google.com/maps/place/Aleksandra+Čaka+iela+100,+Latgales+priekšpilsēta,+Rīga,+LV-1011/@56.9571881,24.1400531,17z/data=!3m1!4b1!4m6!3m5!1s0x46eece343a2aef39:0x23a3f6aea0fc666a!8m2!3d56.9571881!4d24.1400531!16s%2Fg%2F11mtmlbf4c?entry=ttu')">
+          <script>
+            function redirectToLink(url) {
+              window.location.href = url;
+            }
+          </script>
+
+          <h3>INFO</h3>
+          <p>Par mums:</p>
+          <p>Juridiskā adrese: Rīga, Aleksandra Čaka 100</p>
+          <p>Kontakti: +37128282828</p>
+          <p>Rezervācija: pa pastu witchcraft@inbox.lv</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div id="donate-section">
+          <a href="https://www.donationalerts.com" class="donate-button">Donate</a>
+          <img src="images/donate.png" alt="Logo">
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div id="card-section">
+          <div class="card-image">
+            <p>YOUR CARD OF THE DAY!</p>
+            <img src="images/card.png" alt="Card">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
 </body>
 </html>
