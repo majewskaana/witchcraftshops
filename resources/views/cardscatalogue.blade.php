e9eaa089a707cd4a26899833c71a1238799102a6
majewskaana <a.majevska@inbox.lv>
2023-06-19 19:36:52 +0300

    modified:   public/styles.css 	modified:   resources/views/cardscatalogue.blade.php 	modified:   resources/views/login.blade.php 	modified:   resources/views/spicescatalogue.blade.php 	modified:   resources/views/stonecatalogue.blade.php

--- resources/views/cardscatalogue.blade.php
+++ resources/views/cardscatalogue.blade.php
@@ -4,10 +4,30 @@
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <link rel="stylesheet" href="styles.css">
  <title>Cards</title>
 </head>
 <body>
-    <h1>Card catalogue</h1>
+    <h1>Card catalogue</h1> 
+<section class="filter">
+  <h2>Filter</h2>
+  <div class="filter-group">
+    <ul>
+      <li><button type="button" name="filter" value="stones">Stones</button></li>
+      <li><button type="button" name="filter" value="zodiac">Zodiac</button></li>
+    </ul>
+  </div>
+  <div class="filter-group">
+    <h3><button type="button" name="filter" value="spices">Spices</button></h3>
+    <ul>
+      <li><button type="button" name="filter" value="smell">Smell</button></li>
+    </ul>
+  </div>
+  <div class="filter-group">
+    <h3><button type="button" name="filter" value="cards">Cards</button></h3>
+  </div>
+</section>
+
     @if ($cards === null || count($cards) === 0)
         <p class='error'>There are no records in the database!</p>
     @else
