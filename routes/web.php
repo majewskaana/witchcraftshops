4436cbb6509226b034bafa55df4443e04c49ddd8
majewskaana <a.majevska@inbox.lv>
2023-06-19 23:09:11 +0300

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -27,11 +27,4 @@ Route::get('/auth', function () {
 Route::get('/register', function () {
     return view('register');
 });
-Route::get('/auth', function () {
-    return view('login');
-});
 
-
-Route::get('/register', function () {
-    return view('register');
-});
